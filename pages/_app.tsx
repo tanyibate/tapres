2ff394ac2ff9e55b0f593aa57164615274083d02
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2023-02-21 15:21:13 +0000

    updated drfit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { InjectScriptElement as InjectDriftScript } from "../drift/drift";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <InjectDriftScript />
       <Navbar />
       <Component {...pageProps} />
     </>

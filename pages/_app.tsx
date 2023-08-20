fe3147b0ba00199d5862f526446daa38fe8267ca
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2023-08-20 10:11:11 +0100

    lazy load drift script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import Navbar from "@/components/navbar/Navbar";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { InjectScriptElement as InjectDriftScript } from "../drift/drift";
+import { lazy, Suspense } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Lazy load drift script
+  const InjectDriftScript = lazy(() =>
+    import("@/drift/drift").then((mod) => ({
+      default: mod.InjectScriptElement,
+    }))
+  );
+
   return (
     <>
-      <InjectDriftScript />
+      <Suspense fallback={<div>Loading...</div>}>
+        <InjectDriftScript />
+      </Suspense>
       <Navbar />
       <Component {...pageProps} />
     </>
